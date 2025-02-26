56963f4f47f82c952b18692f118394f39cdd755a
Omkar Gaikwad <ogaikwad200@gmail.com>
2025-02-26 10:36:37 +0530

    customer dashboard feature added

--- accounts/views.py
+++ accounts/views.py
@@ -11,7 +11,7 @@ from django.core.exceptions import PermissionDenied
 from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
 from django.template.defaultfilters import slugify
-
+from orders.models import Order
 # restrict vendor from accessing customer page
 def check_role_vendor(user):
     if user.role==1:
@@ -147,7 +147,14 @@ def myAccount(request):
 @login_required(login_url='login')
 @user_passes_test(check_role_customer)
 def custDashboard(request):
-    return render(request,"accounts/custDashboard.html")
+    orders=Order.objects.filter(user=request.user,is_ordered=True)
+    recent_orders=orders[:5]
+    context={
+        'orders':orders,
+        'recent_orders':recent_orders,
+        'orders_Count':orders.count()
+    }
+    return render(request,"accounts/custDashboard.html",context)
 
 @login_required(login_url='login')
 @user_passes_test(check_role_vendor)
--- customers/views.py
+++ customers/views.py
@@ -4,6 +4,8 @@ from accounts.models import User,UserProfile
 from accounts.forms import UserProfileForm,UserInfoForm
 from django.shortcuts import get_object_or_404
 from django.contrib import messages
+from orders.models import Order,OrderedFood
+import simplejson as json
 
 @login_required(login_url='login')
 def cprofile(req):
@@ -36,3 +38,36 @@ def cprofile(req):
 
 
     return render(req,'customers/cprofile.html',context)
+
+@login_required(login_url='login')
+def my_orders(request):
+    orders=Order.objects.filter(user=request.user,is_ordered=True).order_by('-created_at')
+    context={
+        'orders':orders,
+    }
+    return render(request,"customers/my_orders.html",context)
+
+
+@login_required(login_url='login')
+def order_detail(request,order_number):
+    try:
+        order=Order.objects.get(order_number=order_number,is_ordered=True)
+        ordered_food=OrderedFood.objects.filter(order=order)
+        subtotal=0
+        for i in ordered_food:
+            subtotal+=(i.price*i.quantity)
+        tax_data=json.loads(order.tax_data)
+
+        context={
+            'order':order,
+            'ordered_food':ordered_food,
+            'subtotal':subtotal,
+            'tax_data':tax_data
+        }
+        return render(request,"customers/order_detail.html",context)
+    except:
+        return redirect('customer')
+
+    
+
+
